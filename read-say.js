a43da1b72790dad4d666fb964c7f39b89feaba71
Aaron Gray <aaron@hellometa.com>
2015-10-10 12:52:32 -0600

    Include speed control

--- read-say.js
+++ read-say.js
@@ -4,15 +4,16 @@
 
 var extractor = require('unfluff'),
     request = require('request'),
-    say = require('say'),
+    say = require('say-again.js'),
     argv = require('minimist')(process.argv.slice(2))
 
-var voice = argv['v'] || null
+var voice = argv['v'] || null,
+    speed = argv['r'] || null
 
 request(argv['_'][0], function (error, response, body) {
   if (!error && response.statusCode === 200) {
     var data = extractor(body)
-    say.speak(voice, data.text)
+    say.speak(voice, data.text, null, speed)
   } else {
     console.log(error)
   }
